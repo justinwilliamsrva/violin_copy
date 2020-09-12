b8cf1c7fa3347d034a048cb6d40d24dc450a09e9
justinwilliamsrva <justin.williams2@me.com>
2020-09-12 09:46:03 -0400

    connection

--- config/config.js
+++ config/config.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.LOCAL_USERNAME,
-    password: process.env.LOCAL_PASSWORD,
-    database: process.env.LOCAL_DATABASE,
-    host: "127.0.0.1",
+    username: "boi4l8tkglm0k2h8",
+    password: "s42bvchd1u9kea5s",
+    database: "dnam8nx9wdqay9r5",
+    host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+
+    // username: process.env.LOCAL_USERNAME,
+    // password: process.env.LOCAL_PASSWORD,
+    // database: process.env.LOCAL_DATABASE,
+    // host: "127.0.0.1",
+    // host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
